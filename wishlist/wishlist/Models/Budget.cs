7699dea9bd788db1302cae1ca1375daface62b82
Jacob <jakexmueller@gmail.com>
2018-07-28 12:35:41 -0500

    added budget model

---
+++ wishlist/wishlist/Models/Budget.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace wishlist.Models
+{
+    public class Budget
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int WeeklyWage { get; set; }
+        public int Bills { get; set; }
+        public int Groceries { get; set; }
+        public int Transportation { get; set; }
+        public int GoingOutFund { get; set; }
+
+    }
+}
